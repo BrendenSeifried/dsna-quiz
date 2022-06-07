63cfacd0e748ef5ed0f311394f8456592084a98a
BrendenSeifried <brendenseifried@gmail.com>
2022-06-07 15:09:27 -0700

    first

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-class Stack {}
+// class Stack {
+//   constructor(id, name) {
+//     this.id = id;
+//     this.name = name;
+//   }
+//   data = () => {
+//     console.log(`${this.name} has the ID of: ${this.id} `);
+//   };
+// }
 
+// const stuff = new Stack(5, "example");
+// console.log(stuff.data);
+// stuff.data();
+
+class Stack {
+  data = [];
+
+  //   constructor(test) {
+  //     if (test) this.data = test;
+  //     console.log(test);
+  //   }
+}
+const answ = new Stack();
+console.log(answ);
 class Queue {}
 
 module.exports = { Stack, Queue };
